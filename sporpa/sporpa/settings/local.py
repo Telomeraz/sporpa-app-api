3b85d754968b61686e07cc2eb0226279bbe67373
Telomeraz <huseyinemre_armagan@hotmail.com>
2022-07-12 20:36:58 +0300

    Create project

---
+++ sporpa/sporpa/settings/local.py
@@ -0,0 +1,23 @@
+from decouple import config
+
+from .base import *
+
+SECRET_KEY = config("SECRET_KEY")
+
+DEBUG = True
+
+INSTALLED_APPS += ("django_extensions",)
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
