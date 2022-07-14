6f942315b38068816b1e21c2c8a54b3d4983acb2
Telomeraz <huseyinemre_armagan@hotmail.com>
2022-07-14 21:03:09 +0300

    Add Pillow for the sake of media

--- sporpa/core/settings/base.py
+++ sporpa/core/settings/base.py
@@ -94,6 +94,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 # DRF Settings
 
--- sporpa/core/urls.py
+++ sporpa/core/urls.py
@@ -1,5 +1,7 @@
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
