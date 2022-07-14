3c858c5eca0df52d51b31a79e700dc9ecc4947a2
Telomeraz <huseyinemre_armagan@hotmail.com>
2022-07-14 11:45:00 +0300

    Activate TokenAuthentication

--- sporpa/core/settings/base.py
+++ sporpa/core/settings/base.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_spectacular",
     "accounts",
 ]
@@ -97,7 +98,7 @@ STATIC_URL = "/static/"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
