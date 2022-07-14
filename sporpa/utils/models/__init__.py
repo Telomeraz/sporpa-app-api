54217ff4abdda470899964891b17003db993285c
Telomeraz <huseyinemre_armagan@hotmail.com>
2022-07-14 20:59:07 +0300

    Add utils app and define TrackingMixin model

---
+++ sporpa/utils/models/__init__.py
@@ -0,0 +1,3 @@
+from .tracking_mixin import TrackingManagerMixin, TrackingMixin
+
+__all__ = ["TrackingMixin", "TrackingManagerMixin"]
---
+++ sporpa/utils/models/tracking_mixin.py
@@ -0,0 +1,36 @@
+from typing import Dict, Tuple
+
+from django.db import models
+from django.db.models import QuerySet
+from django.utils.translation import gettext_lazy as _
+
+
+class TrackingManagerMixin(models.Manager):
+    def __init__(self, *args: Tuple, **kwargs: Dict) -> None:
+        self.all_objects = kwargs.pop("all_objects", False)
+        super().__init__(*args, **kwargs)
+
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        if self.all_objects:
+            return queryset
+        return queryset.filter(deleted_at__isnull=True)
+
+
+class TrackingMixin(models.Model):
+    created_at = models.DateTimeField(
+        _("created at"),
+        auto_now_add=True,
+    )
+    deleted_at = models.DateTimeField(
+        _("deleted at"),
+        default=None,
+        help_text=_(
+            "Designates whether this user should be treated as active. Enter a datetime instead of deleting user.",
+        ),
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        abstract = True
