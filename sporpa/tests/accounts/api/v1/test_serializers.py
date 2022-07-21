b9861c1ec4964868e5df5db23c7057192c2f6a44
Telomeraz <huseyinemre_armagan@hotmail.com>
2022-07-21 20:04:52 +0300

    Fix naming convention

--- sporpa/tests/accounts/api/v1/test_serializers.py
+++ sporpa/tests/accounts/api/v1/test_serializers.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.django_db
 
 
 class TestAuthTokenSerializer:
-    def test_auth_token_serializer(self, user: User) -> None:
+    def test_validate(self, user: User) -> None:
         data = {
             "email": user.email,
             "password": "testpassword",
@@ -18,14 +18,14 @@ class TestAuthTokenSerializer:
         assert serializer.validated_data["password"] == "testpassword"
         assert serializer.validated_data["user"] == user
 
-    def test_auth_token_serializer_when_no_password(self) -> None:
+    def test_validate_when_no_password(self) -> None:
         data = {
             "email": "qweasd@example.com",
         }
         serializer = AuthTokenSerializer(data=data)
         assert serializer.is_valid() is False
 
-    def test_auth_token_serializer_user_doesnt_exist(self) -> None:
+    def test_validate_user_does_not_exist(self) -> None:
         data = {
             "email": "qweasd@example.com",
             "password": "qweasd",
@@ -33,7 +33,7 @@ class TestAuthTokenSerializer:
         serializer = AuthTokenSerializer(data=data)
         assert serializer.is_valid() is False
 
-    def test_auth_token_serializer_when_user_is_passive(self, passive_user: User) -> None:
+    def test_validate_when_user_is_passive(self, passive_user: User) -> None:
         data = {
             "email": passive_user.email,
             "password": "testpassword",
