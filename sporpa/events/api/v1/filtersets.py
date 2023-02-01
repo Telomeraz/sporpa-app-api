0d294df645e991a134af15adaef346b6c7cecc94
Telomeraz <huseyinemre_armagan@hotmail.com>
2023-02-01 18:36:41 +0300

    Refactor ActivityListFilterset for the sake of ActivityListView

--- sporpa/events/api/v1/filtersets.py
+++ sporpa/events/api/v1/filtersets.py
@@ -1,12 +1,13 @@
 from django_filters import rest_framework as filters
 
 from django.db.models import QuerySet
+from django.utils.translation import gettext_lazy as _
 
 from events.models import Activity
 from participants.models import Sport
 
 
-class ActivityListFilterset(filters.FilterSet):
+class BaseActivityListFilterset(filters.FilterSet):
     sport = filters.ModelMultipleChoiceFilter(
         field_name="sport",
         queryset=Sport.objects.all(),
@@ -15,20 +16,6 @@ class ActivityListFilterset(filters.FilterSet):
         "available_between_at",
         method="_filter_available_between_at",
     )
-    status = filters.TypedMultipleChoiceFilter(
-        field_name="status",
-        choices=Activity.Status.choices,
-        coerce=int,
-    )
-
-    class Meta:
-        model = Activity
-        fields = (
-            "sport",
-            "levels",
-            "available_between_at",
-            "status",
-        )
 
     def _filter_available_between_at(
         self,
@@ -40,4 +27,56 @@ class ActivityListFilterset(filters.FilterSet):
             queryset = queryset.filter(available_between_at__gt=(value.start, None))
         if value.stop:
             queryset = queryset.filter(available_between_at__lt=(value.stop, None))
+        return queryset
+
+
+class ActivityListFilterset(BaseActivityListFilterset):
+    class Meta:
+        model = Activity
+        fields = (
+            "sport",
+            "levels",
+            "available_between_at",
+        )
+
+
+class ParticipatedActivityListFilterset(BaseActivityListFilterset):
+    PT_ORGANIZER = "organizer"
+    PT_PARTICIPANT = "participant"
+    PLAYER_TYPES = (
+        (PT_ORGANIZER, _("Organizer")),
+        (PT_PARTICIPANT, _("Participant")),
+    )
+
+    status = filters.TypedMultipleChoiceFilter(
+        field_name="status",
+        choices=Activity.Status.choices,
+        coerce=int,
+    )
+    player_type = filters.TypedChoiceFilter(
+        method="_filter_player_type",
+        choices=PLAYER_TYPES,
+        coerce=str,
+    )
+
+    class Meta:
+        model = Activity
+        fields = (
+            "sport",
+            "levels",
+            "available_between_at",
+            "status",
+            "player_type",
+        )
+
+    def _filter_player_type(
+        self,
+        queryset: QuerySet[Activity],
+        name: str,
+        value: str,
+    ) -> QuerySet[Activity]:
+        if value == self.PT_ORGANIZER:
+            queryset = queryset.filter_organizer(self.request.user.player)
+        elif value == self.PT_PARTICIPANT:
+            queryset = queryset.filter_participant(self.request.user.player)
         return queryset
